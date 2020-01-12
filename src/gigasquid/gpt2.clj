f198cb7fe37877e0d804ed665fe534c2311580fc
gigasquid <cmeier@gigasquidsoftware.com>
2020-01-11 22:31:48 -0500

    Added better generation with temperature to get rid of repititions

--- src/gigasquid/gpt2.clj
+++ src/gigasquid/gpt2.clj
@@ -66,7 +66,7 @@ predicted-index ;=>582
 
 (defn generate-sequence-step [{:keys [generated-tokens context past]}]
   (let [[output past] (model context :past past)
-        token (-> (torch/argmax (first output)))
+        token (torch/argmax (first output))
         new-generated  (conj generated-tokens (py/$a token tolist))]
     {:generated-tokens new-generated
      :context (py/$a token unsqueeze 0)
@@ -104,10 +104,57 @@ predicted-index ;=>582
                 (range num-of-words-to-predict))]
     (decode-sequence result)))
 
-(generate-text "Clojure is a dynamic, general purpose programming language, combining the approachability and interactive" 20)
+(generate-text "Natural language processing tasks are typically approached with" 
+               100)
 
 ;=> "Clojure is a dynamic, general purpose programming language, combining the approachability and interactive. It is a language that is easy to learn and use, and is easy to use for anyone"
 
 
 
+;;;;;; Better sequence generating
+;;; With temperature to get rid of repititions
+
+;;; from https://github.com/huggingface/transformers/issues/1725
+
+(require-python '(torch.nn.functional))
+
+(defn sample-sequence-step [{:keys [generated-tokens context past temp]
+                             :or {temp 0.8}}]
+  (let [[output past] (py/with [r (torch/no_grad)]
+                       (model context :past past))
+        next-token-logits (torch/div (-> output first last)
+                                     (if (pos? temp) temp 1))
+        token (torch/multinomial
+               (torch.nn.functional/softmax next-token-logits :dim -1) :num_samples 1)
+        new-generated  (conj generated-tokens (first (py/$a token tolist)))]
+    {:generated-tokens new-generated
+     :context (py/$a token unsqueeze 0)
+     :past past
+     :token token}))
+
+(defn generate-text2 [starting-text num-of-words-to-predict temp]
+  (let [tokens (into [] (py/$a tokenizer encode starting-text))
+        context (torch/tensor [tokens])
+        result (reduce
+                (fn [r i]
+                  (println i)
+                  (sample-sequence-step (assoc r :temp temp)))
+
+                {:generated-tokens tokens
+                 :context context
+                 :past nil}
+
+                (range num-of-words-to-predict))]
+    (decode-sequence result)))
+
+(generate-text2 "Natural language processing tasks are typically approached with" 
+                100
+                0.8)
+
+;>"Natural language processing tasks are typically approached with distress signals and pleasurable stimuli.\n\n7.2.3. Structural networks\n\nStructural networks are comprised of various layers of information that are coupled with instructions for performing behavioral tasks. Such networks can be used for e.g., associating individual groups with special differential activities (e.g., listening to music, studying a subject's handwriting), or for performing complex tasks such as reading and writing a chart. The presence of structures that are familiar to the participant may also help"
+
+(generate-text2 "It is thought that cheese was first discovered around 8000 BC around the time when sheep were first domesticated"
+                100
+                0.8)
+;=>"It is thought that cheese was first discovered around 8000 BC around the time when sheep were first domesticated as sheep. Native American plants and animals associated with such plants are described as being \"mushy, leafy and musky\" from having \"powder-like stalks and narrow niche-like leaves.\" They are believed to have been found in the Cauca Chaco area of South America and northern Mexico. The earliest known cases of cheese in the Americas could be traced back to around 160 BC, when the deposits of the Cauca Chaco were discovered in Colombia, Peru and Argentina"
 
