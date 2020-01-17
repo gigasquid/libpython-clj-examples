288857e4b1488d0e3139d1ee43d0a5de7bb4b047
gigasquid <cmeier@gigasquidsoftware.com>
2020-01-17 17:38:44 -0500

    Matplotlib from another namespace

--- src/gigasquid/plot.clj
+++ src/gigasquid/plot.clj
@@ -19,39 +19,21 @@
 (require-python 'numpy)
 
 
-(def fig (matplotlib.pyplot/figure))
-(def agg-canvas (matplotlib.backends.backend_agg/FigureCanvasAgg fig))
-
-
-
-(defn plot []
-  (do
-    (matplotlib.pyplot/plot [[1 2 3 4 5] [1 2 3 4 10]] "go")
-   (let [_ (py. agg-canvas "draw")
-         np-data (numpy/array (py. agg-canvas "buffer_rgba"))
-         tens (py/as-tensor np-data)
-         bufimage (bufimg/new-image 480 640 :byte-abgr)]
-     (dtt/select tens :all :all (->> (range 4) reverse))
-     (dtype/copy! tens bufimage)
-     (bufimg/save! bufimage "temp.png")
-     (sh/sh "open" "temp.png"))))
-
-
-(defn pyplot
-  "Returns the configured pyplot module to use from other namespaces"
-  [])
-
-
 (defmacro with-show
   "Takes forms with mathplotlib.pyplot to then show locally"
   [& body]
-  `(let [fig# (matplotlib.pyplot/figure)
-        agg-canvas# (matplotlib.backends.backend_agg/FigureCanvasAgg fig#)
-        ignored#  ~(cons 'do body)
-        ignored# (py. agg-canvas# "draw")
-        np-data# (numpy/array (py. agg-canvas# "buffer_rgba"))
-        tens# (py/as-tensor np-data#)
-        bufimage# (bufimg/new-image 480 640 :byte-abgr)]
+  `(let [_# (matplotlib.pyplot/clf)
+         fig# (matplotlib.pyplot/figure)
+         agg-canvas# (matplotlib.backends.backend_agg/FigureCanvasAgg fig#)
+         _#  ~(cons 'do body)
+         _# (py. agg-canvas# "draw")
+         np-data# (numpy/array (py. agg-canvas# "buffer_rgba"))
+         tens# (py/as-tensor np-data#)
+         shape# (dtype/shape tens#)
+         h# (first shape#)
+         w# (second shape#)
+         bufimage# (bufimg/new-image h# w# :byte-abgr)]
+     (print "shape " shape# "h" h# "w" w#)
     (dtype/copy! tens# bufimage#)
     (dtt/select tens# :all :all (->> (range 4) reverse))
     (bufimg/save! bufimage# "temp.png")
@@ -69,24 +51,9 @@
     (matplotlib.pyplot/ylabel "y label")
     (matplotlib.pyplot/title "Simple Plot"))
 
+
   (with-show (matplotlib.pyplot/plot [[1 2 3 4 5] [1 2 3 4 10]] :label "linear"))
   )
-
-(comment
-
-  (let [fig (matplotlib.pyplot/figure)
-        agg-canvas (matplotlib.backends.backend_agg/FigureCanvasAgg fig)
-        ignored  (matplotlib.pyplot/plot [[1 2 3 4 5] [1 2 3 4 10]] :label "linear")
-        ignored (py. agg-canvas "draw")
-        np-data (numpy/array (py. agg-canvas "buffer_rgba"))
-        tens (py/as-tensor np-data)
-        bufimage (bufimg/new-image 480 640 :byte-abgr)]
-    (dtype/copy! tens bufimage)
-    (dtt/select tens :all :all (->> (range 4) reverse))
-    (bufimg/save! bufimage "temp.png")
-    (sh/sh "open" "temp.png"))
-  )
-;;; now the rendering
 
 
 
